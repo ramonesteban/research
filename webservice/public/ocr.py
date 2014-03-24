b6d8ea536870d0c903ac4ad13d988b4a3a0acbbf
Ramón González <ramonesteban.gzz@gmail.com>
2014-03-23 18:59:24 -0600

    verify if file exists

--- webservice/public/ocr.py
+++ webservice/public/ocr.py
@@ -1,5 +1,9 @@
+import os
 import Image
 from pytesser import *
 
-image = Image.open('image.jpg')
-print image_to_string(image)
+if os.path.isfile('public/image.jpg'):
+  image = Image.open('public/image.jpg')
+  print image_to_string(image)
+else:
+  print 'File not found.'
