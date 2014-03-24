05140159564e9f31311a7339cbf77fa76510259f
Ramón González <ramonesteban.gzz@gmail.com>
2014-03-23 19:04:27 -0600

    validate presence of params

--- webservice/app/models/product.rb
+++ webservice/app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  validates :name,
+    presence: true,
+    length: { minimum: 2, maximum: 40 }
+
+  validates :company,
+    presence: true
+
+  validates :origin,
+    presence: true
+
+  validates :description,
+    presence: true
+
+  validates :rate,
+    presence: true
 end
