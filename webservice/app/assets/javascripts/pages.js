3061bac34fc0c20511e1ff5e680b321756749a31
Ramón González <ramonesteban.gzz@gmail.com>
2014-04-28 21:45:37 -0500

    preview image before uploading

--- webservice/app/assets/javascripts/pages.js
+++ webservice/app/assets/javascripts/pages.js
@@ -1,2 +1,18 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#preview-image').attr('src', e.target.result);
+      $('#preview-container').show();
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(document).ready(function() {
+  $('#picture').change(function() {
+    readURL(this);
+  });
+});
