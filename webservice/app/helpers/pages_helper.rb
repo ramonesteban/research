65a3fa036d35980e954a7fd268cf257c09fd0f03
Ramón González <ramonesteban.gzz@gmail.com>
2014-05-14 22:35:41 -0500

    changed helper and ignored images in pictures folder

--- webservice/app/helpers/pages_helper.rb
+++ webservice/app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def get_image_text(filename)
-    path = Rails.root.join('public').to_s
+    path = Rails.root.join('python').to_s
     response = %x(python #{path}/ocr.py public/pictures/#{filename})
   end
 end
