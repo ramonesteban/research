e4b6ad44a3c5d338564af6e9ed2cc1f4620c0289
Ramón González <ramonesteban.gzz@gmail.com>
2014-05-23 23:11:46 -0500

    moved file and fix inconsistencies

--- tryout/improvement.py
+++ tryout/improvement.py
@@ -1,30 +1,28 @@
 import sys, os
 import cv2
 import numpy as np
-from pytesser import *
 
+# Hack to use modules in parent folder
+sys.path.append('..')
+from pytesser import *
 from general_functions import *
 
-def improvement_test(image_file_path):
+def improvement(image_file_path):
     image = cv2.imread(image_file_path)
     gray_image_array = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # ret, thresh = cv2.threshold(gray_image_array, 127, 255, cv2.THRESH_BINARY_INV)
     ret, thresh = cv2.threshold(gray_image_array, 127, 255, 0)
-
     kernel = np.ones((5,5), np.uint8)
     erosion = cv2.erode(thresh, kernel, iterations = 1)
 
     show_image_in_window(thresh)
-
     print get_text_from_image(thresh)
-
 
 def main():
     if len(sys.argv) > 1:
         image_file_path = sys.argv[1]
         if os.path.isfile(image_file_path):
-            improvement_test(image_file_path)
+            improvement(image_file_path)
         else:
             print 'Image file does not exist'
     else:
